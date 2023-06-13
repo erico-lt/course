d137227e6d6069faaa061bf1e2ca78c1820692b8
Erico Santana <erico.blp@gmail.com>
2023-06-12 23:43:09 -0300

    Category an CategoryRepository

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -8,15 +8,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entites.Category;
 import com.educandoweb.course.entites.Order;
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.enums.OrderStatus;
+import com.educandoweb.course.repositorys.CategoryRepository;
 import com.educandoweb.course.repositorys.OrderRepository;
 import com.educandoweb.course.repositorys.UserRepository;
 
 @Configuration
 @Profile("teste")
 public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -26,13 +31,19 @@ public class TestConfig implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
         User u1 = new User(null, "Érico", "erico.blp@gmail.com", "2325666", "12345");
         User u2 = new User(null, "Olivia", "olivia.blp@gmail.com", "5453656", "232332");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAINTING);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAINTING);
-        
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
