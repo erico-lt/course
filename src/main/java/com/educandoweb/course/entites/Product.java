0c89a60e7b30fcd3177e5a231648ea48b2b93eea
Erico Santana <erico.blp@gmail.com>
2023-06-30 22:16:29 -0300

    OneToMany Product for Order

--- src/main/java/com/educandoweb/course/entites/Product.java
+++ src/main/java/com/educandoweb/course/entites/Product.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +33,9 @@ public class Product implements Serializable {
     @JsonIgnore
     @JoinTable(name = "tb_category_product", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.products")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -86,6 +90,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+
+        return set;
     }
 
     @Override
