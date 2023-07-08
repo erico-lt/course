284ec347f83f45f7720ce0c30f313a08b3fe9c28
Erico Santana <erico.blp@gmail.com>
2023-07-07 22:21:22 -0300

    Atualizando o erro do ResponseEntity, era problema na minha rede

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.resources.exceptions;
 import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,17 +18,18 @@ import jakarta.servlet.http.HttpServletRequest;
  */
 
 @RestControllerAdvice
-public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{     
+    
     @ExceptionHandler(ResourceNotFoundException.class) // Anotação para interceptar exceções do tipo ResourceNotFound
-    public String resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND; // Usado para mudar o tipo de codigo vai ser apresentado caso estoure uma exceção
         
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); //Este é o corpo que vai aparecer caso não encontre um usuario
 
-        return err.toString();
+        return ResponseEntity.status(status).body(err);
     }
+     
+     
 }
