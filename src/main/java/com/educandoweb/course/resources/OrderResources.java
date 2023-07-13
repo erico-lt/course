fcccc6f57dec9f57f48160dc7d3d80c9dd57f67b
Erico Santana <erico.blp@gmail.com>
2023-07-13 19:33:46 -0300

    Create method delete Order

--- src/main/java/com/educandoweb/course/resources/OrderResources.java
+++ src/main/java/com/educandoweb/course/resources/OrderResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,12 @@ public class OrderResources {
         order = orderServices.insert(order);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(order.getId()).toUri();
         return ResponseEntity.created(uri).body(order);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        orderServices.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/educandoweb/course/services/OrderServices.java
+++ src/main/java/com/educandoweb/course/services/OrderServices.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entites.Order;
 import com.educandoweb.course.repositorys.OrderRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,5 +30,18 @@ public class OrderServices {
     public Order insert(Order order) {
 
         return orderRepository.save(order);
+    }
+
+    public void delete(Long id) {
+        
+        try {
+            if(!orderRepository.existsById(id)) {
+                throw new ResourceNotFoundException(id);
+            } else {
+                orderRepository.deleteById(id);
+            }            
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
